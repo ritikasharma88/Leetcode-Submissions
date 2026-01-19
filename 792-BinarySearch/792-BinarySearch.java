095bbadace452daba49551642a8e016211970b69
ritikasharma88 <ritika.sharma_cs.csf23@gla.ac.in>
2026-01-19 22:35:47 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,23 @@
+// Last updated: 1/19/2026, 10:35:47 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left<= right){
+            int mid =  left + (right-left)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid]<target){
+                left = mid +1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return -1;
+        
+    }
+}
